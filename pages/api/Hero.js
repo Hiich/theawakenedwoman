bd336ae8c43174bc5ba694b1144ce42e0b283df9
Ebiwari Meshach <ebiwarimeshach@gmail.com>
2022-08-04 04:48:29 +0100

    logo update

--- pages/api/Hero.js
+++ pages/api/Hero.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
         Hero_Content: "The Awakened Woman is a thriving community focused on empowering women to live their most magnificent lives in all areas; Physically, Mentally, Spiritually & Financially As the lion’s portal opens on the 8th day of the 8th month, Gaia is activating 5,555 women to Awaken as the Keepers of earth for her & humanity’s survival.",
         HeroStatsOne:{
             title: "Minting",
-            value: "8/8/12"
+            value: "8/8/22"
         },
         HeroStatsTwo:{
             title: "Collection",
